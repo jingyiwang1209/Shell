d707795dc7467f2b78a98c87ba9e256291131f86
jisaacs <shizuwang1209@gmail.com>
2018-12-26 16:53:05 -0900

    Fixed string redirect

--- show-attackers.sh
+++ show-attackers.sh
@@ -7,23 +7,16 @@ if [[ ! -e "${1}" ]]
 fi
 
 echo 'Count, IP, Location'
-grep Failed ${1} | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr | while read COUNT IP
+IPS_COUNTED=grep Failed ${1} | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr
+while read COUNT IP
 do
 if [[ "${COUNT}" -gt 10 ]]
 then
   LOCATION=$(geoiplookup ${IP} | awk -F ',' '{print $2}')
   echo "${COUNT}, ${IP}, {LOCATION}"
 fi
-done
+done <<< "${IP_COUNTED}"
 exit 0
-
-# Alternative
-IPS_COUNTED=$(grep Failed ${1} | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr)
-
-while read COUNT IP
-do
- ...
-done < IPS_COUNTED
   
 
 
